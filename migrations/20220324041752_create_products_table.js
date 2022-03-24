741b59d98305d559715167dfa2633c4539bb676e
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-03-23 23:22:51 -0600

    Initial commit

---
+++ migrations/20220324041752_create_products_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('bicycles', (table) => {
+        table.increments('id');
+        table.string('color', 255).notNullable();
+        table.string('modelo', 255).notNullable();
+        table.decimal('latitude', 10, 8).notNullable();
+        table.decimal('longitude', 11, 8).notNullable();
+        table.timestamps(true, true);
+      });
+};
+  
+exports.down = function(knex) {
+    return knex.schema
+      .dropTable('bicycles');
+};
